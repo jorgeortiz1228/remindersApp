2e052597232e73460f822ed60e7dd71fe55dd2b9
jorgeortiz1228 <jorgeortiz1228@gmail.com>
2019-12-19 17:26:02 -0800

    initial committ

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container">
+    <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
+    <new-quote @quoteAdded="newQuote"></new-quote>
+    <quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></quote-grid>
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <div class="alert alert-info">Tip : Click on a reminder to delete it !</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import QuoteGrid from './components/QuoteGrid.vue';
+import NewQuote from './components/NewQuote.vue';
+import Header from './components/Header.vue';
+
+export default {
+  data: function() {
+    return {
+      quotes: [
+        'Just a sample reminder .'
+      ],
+      maxQuotes: 10
+    };
+  },
+  methods: {
+    newQuote(quote) {
+      if (this.quotes.length >= this.maxQuotes) {
+        return alert('Delete a reminder before adding a new one .');
+      }
+      this.quotes.push(quote);
+    },
+    deleteQuote(i) {
+      this.quotes.splice(i, 1);
+    }
+  },
+  components: {
+    'quote-grid': QuoteGrid,
+    'new-quote': NewQuote,
+    'app-header': Header
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/NewQuote.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="row">
+        <form>
+            <div class="col-sm-8 col-sm-offset col-xs-12 col-md-6 col-md-offset-3 form-group">
+                <label>Reminder</label>
+                <textarea rows="3" class="form-control" v-model="quote"></textarea>
+            </div>
+            <div class="col-sm-8 col-sm-offset col-xs-12 col-md-6 col-md-offset-3 form-group">
+                <button class="btn btn-primary" @click.prevent="createNew">Add Reminder</button>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+
+
+export default {
+    data: function() {
+        return {
+            quote: ''
+        };
+    },
+    methods: {
+        createNew() {
+            this.$emit('quoteAdded', this.quote);
+            this.quote = '';
+        }
+    }
+}
+</script>
